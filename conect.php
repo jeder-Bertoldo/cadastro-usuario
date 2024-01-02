0cd0d6bbc17b433c0068205f058d142ffdec6db2
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-02 09:43:33 -0500

    Arquivo PHP apenas para estabelecer a conexão

---
+++ conect.php
@@ -0,0 +1,14 @@
+<?php
+// Configuração da conexão com o banco de dados
+$host = "localhost"; // ou o endereço do seu servidor de banco de dados
+$dbname = "cadastro-user"; // nome do banco de dados
+$username = "root"; // usuário do banco de dados
+$password = ""; // senha do banco de dados
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Checar conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+?>
